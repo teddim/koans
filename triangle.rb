79c04ea17aae761c22dc5242c7a17710c0b05a5d
Teddi Maull <teddi@picturetheday.com>
2015-03-01 19:00:32 -0700

    More Koan progress

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,22 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+
+  raise TriangleError, "Sides can't be zero" if a == 0 || b == 0 || c == 0
+  raise TriangleError, "Sides can't be negative" if a < 0 || b < 0 || c < 0
+  raise TriangleError, "Not a valid triangle" if a + b <= c || b + c <= a || a + c <= b
+
+  if a == b && b == c
+      :equilateral
+    else if a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+
 end
